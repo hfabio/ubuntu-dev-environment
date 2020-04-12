79f2a885bb08f94c3cd49bfc200d49c19532598e
Helton Fábio <heltonfabio@outlook.com>
2020-04-12 00:29:48 -0300

    fixing node version to 12.x

--- ubuntu-dev-environment.sh
+++ ubuntu-dev-environment.sh
@@ -26,9 +26,7 @@ sudo apt-get install apache2 php7.2 php-mysql php-xdebug -y
 sudo apt-get install mysql-server-5.7 mysql-client-5.7 mysql-workbench mysql-workbench-data -y
 
 # nodejs 12.16.2
-curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-sudo sh -c "echo deb https://deb.nodesource.com/node_12.x bionic main \
-> /etc/apt/sources.list.d/nodesource.list" -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 # npm
