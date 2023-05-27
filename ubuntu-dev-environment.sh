2434d78157f0742d4433a5366ac75a1b61b6cfcb
Helton Fábio <heltonfabio@outlook.com>
2023-05-27 15:17:07 -0300

    update - 2023 - without mysql directly, php and games

--- ubuntu-dev-environment.sh
+++ ubuntu-dev-environment.sh
@@ -43,22 +43,8 @@ wget https://github.com/shiftkey/desktop/releases/download/release-2.4.1-linux1/
 wget -c https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O /tmp/teamviewer.deb
 # google chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google_chrome.deb
-# Insync
-#wget https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.1.2.40785-bionic_amd64.deb -O /tmp/insync_3.1.2.deb
-# Dropbox
-wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb -O /tmp/dropbox.deb
-# skype
-wget https://repo.skype.com/latest/skypeforlinux-64.deb -O /tmp/skypeforlinux.deb
 # discord
 wget https://dl.discordapp.net/apps/linux/0.0.10/discord-0.0.10.deb -O /tmp/discord.deb
-# 4k video downloader
-wget https://dl.4kdownload.com/app/4kvideodownloader_4.9.2-1_amd64.deb -O /tmp/4kvideodownloader.deb
-# notable
-wget https://download.notable.app/\?target\=deb -O /tmp/notable.deb
-# komorebi
-wget https://github.com/cheesecakeufo/komorebi/releases/download/v2.1/komorebi-2.1-64-bit.deb -O /tmp/komorebi.deb
-# create-react-app
-sudo npm i -g create-react-app
 
 # now installing .deb stuff ----------------------------------------------------
 clear_and_print 'now installing .deb stuff'
@@ -70,16 +56,8 @@ clear_and_print 'now installing no important stuff like games or music'
 # removing stuff and adding ppas------------------
 sudo apt-get autoremove gimp gimp-plugin-registry
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y
-sudo add-apt-repository ppa:kritalime/ppa -y
-sudo add-apt-repository ppa:kdenlive/kdenlive-stable -y
-sudo add-apt-repository ppa:lutris-team/lutris -y
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key -y
 sudo apt update
 # end removing stuff and adding ppas------------------
-
-# Nativifier (to create PWAs)
-sudo npm i -g nativefier
 
 # gimp
 sudo apt-get update
@@ -96,28 +74,8 @@ sudo wget https://github.com/microsoft/cascadia-code/releases/download/v1911.21/
 # font menlo for terminal fix
 sudo wget https://github.com/abertsch/Menlo-for-Powerline/blob/master/Menlo%20for%20Powerline.ttf -O /usr/share/fonts/truetype/Menlo\ for\ Powerline.ttf
 
-# Steam
-sudo apt install steam-installer steam-devices steam:i386 -y
-
-# Wine
-sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
-
-# Lutris
-sudo apt-get install lutris libvulkan1 libvulkan1:i386 -y
-
-# krita e kdenlive
-sudo apt-get install krita kdenlive -y
-
-# blanket
-flatpak install flathub com.rafaelmardojai.Blanket
-
 # then the languages/compilers -------------------------------------------------
 clear_and_print 'installing dev stuff'
-# php
-sudo apt-get install apache2 php7.2 php-mysql php-xdebug -y
-
-# mysql
-# sudo apt-get install mysql-server-5.7 mysql-client-5.7 mysql-workbench mysql-workbench-data -y
 
 # installing docker
 clear_and_print 'installing docker'
@@ -126,18 +84,17 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 # docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "Docker and docker compose successfully installed"
 docker --version
 docker-compose --version
 
-# nodejs 12.16.2
 clear_and_print 'installing nvm'
 
 #nvm install
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
 #-------------------------------------------------------------------------------
 # finishing  -------------------------------------------------------------------
@@ -147,7 +104,7 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y &
 # lets install
 clear_and_print 'Lets install zsh with configurated theme\nPay attention, you will need to insert your password here...'
 sudo apt-get install fonts-powerline -y
-wget https://raw.githubusercontent.com/hfabio/zsh-magic-install/master/zsh-magic-install -O /tmp/zsh-magic-install.sh
+wget https://raw.githubusercontent.com/Viniciusalopes/zsh-magic-install/master/zsh-magic-install -O /tmp/zsh-magic-install.sh
 sudo chmod 775 /tmp/zsh-magic-install.sh
 /tmp/zsh-magic-install.sh
 echo 'pay attention to change the theme and add zinit like here https://blog.rocketseat.com.br/terminal-com-oh-my-zsh-spaceship-dracula-e-mais/'
